edc8777c1edaeaeaf84c6cf0c23270cc0b6fcf17
Daimler Garay <Daimler.Garay@priceindustries.com>
2025-05-20 13:13:13 -0500

    Configured oil.nvim

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -2,7 +2,15 @@ return {
   'stevearc/oil.nvim',
   ---@module 'oil',
   ---@type oil.SetupOpts
-  opts = {},
+  keys = {
+    { '\\', '<cmd>Oil --float<CR>', desc = 'Oil reveal', silent = true },
+  },
+  opts = {
+    default_file_explorer = true,
+    view_options = {
+      show_hidden = true,
+    },
+  },
   dependencies = { { 'echasnovski/mini.icons', opts = {} } },
   lazy = false,
 }
