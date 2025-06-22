5ecf9012eea218cb9cdf743303b08c57266ff8c1
Daimler Garay <daimlerchryslergaray@gmail.com>
2025-06-22 18:37:56 -0500

    keybinds for blink

--- lua/kickstart/plugins/blink-cmp.lua
+++ lua/kickstart/plugins/blink-cmp.lua
@@ -58,6 +58,16 @@ return {
         --
         -- See :h blink-cmp-config-keymap for defining your own keymap
         preset = 'default',
+        ['<Tab>'] = { 'snippet_forward', 'fallback' },
+        ['<S-Tab>'] = { 'snippet_backward', 'fallback' },
+        ['<Up>'] = { 'select_prev', 'fallback' },
+        ['<Down>'] = { 'select_next', 'fallback' },
+        ['<C-p>'] = { 'select_prev', 'fallback' },
+        ['<C-n>'] = { 'select_next', 'fallback' },
+        ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
+        ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
+        ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
+        ['<C-e>'] = { 'hide', 'fallback' },
 
         -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
         --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -219,6 +219,7 @@ return {
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         ruff = {},
+        somesass_ls = {},
         html = {},
         cssmodules_ls = {},
         pylsp = {
