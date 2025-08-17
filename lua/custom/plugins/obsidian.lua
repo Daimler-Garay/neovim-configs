0ab9c07352a01ceb5807f2e2d9f510bf4ffb534c
Daimler Garay <daimlerchryslergaray@gmail.com>
2025-08-16 19:13:31 -0500

    merging

--- lua/custom/plugins/obsidian.lua
+++ lua/custom/plugins/obsidian.lua
@@ -21,7 +21,7 @@ return {
     workspaces = {
       {
         name = 'personal',
-        path = 'C:/Users/daimler.garay/Documents/personal',
+        path = '~/vaults/personal',
       },
     },
 
