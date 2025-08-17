2002da54b04c9d5229032bb173fe2ea432991038
Daimler Garay <daimlerchryslergaray@gmail.com>
2025-08-16 19:32:13 -0500

    merging

--- lua/custom/plugins/obsidian.lua
+++ lua/custom/plugins/obsidian.lua
@@ -14,31 +14,33 @@ return {
       {
         name = 'personal',
         path = '~/vaults/personal',
-  opts = function()
-    local vault = vim.fn.expand '~/vaults/notes'
-    return {
-      workspaces = {
-        { name = 'notes', path = vault },
+        opts = function()
+          local vault = vim.fn.expand '~/vaults/notes'
+          return {
+            workspaces = {
+              { name = 'notes', path = vault },
+            },
+            notes_subdir = 'zettel',
+            daily_notes = {
+              folder = 'daily',
+              date_format = '%Y-%m-%d',
+            },
+            templates = {
+              folder = 'templates',
+              date_format = '%Y-%m-%d',
+              time_format = '%H:%M',
+            },
+            disable_frontmatter = false,
+          }
+        end,
+        keys = {
+          { '<leader>on', '<cmd>ObsidianNew<cr>', desc = 'Obsidian: New note' },
+          { '<leader>oq', '<cmd>ObsidianQuickSwitch<cr>', desc = 'Obsidian: Quick switch' },
+          { '<leader>os', '<cmd>ObsidianSearch<cr>', desc = 'Obsidian: Search in vault' },
+          { '<leader>ob', '<cmd>ObsidianBacklinks<cr>', desc = 'Obsidian: Backlinks' },
+          { '<leader>od', '<cmd>ObsidianToday<cr>', desc = 'Obsidian: Today (daily)' },
+        },
       },
-      notes_subdir = 'zettel',
-      daily_notes = {
-        folder = 'daily',
-        date_format = '%Y-%m-%d',
-      },
-      templates = {
-        folder = 'templates',
-        date_format = '%Y-%m-%d',
-        time_format = '%H:%M',
-      },
-      disable_frontmatter = false,
-    }
-  end,
-  keys = {
-    { '<leader>on', '<cmd>ObsidianNew<cr>', desc = 'Obsidian: New note' },
-    { '<leader>oq', '<cmd>ObsidianQuickSwitch<cr>', desc = 'Obsidian: Quick switch' },
-    { '<leader>os', '<cmd>ObsidianSearch<cr>', desc = 'Obsidian: Search in vault' },
-    { '<leader>ob', '<cmd>ObsidianBacklinks<cr>', desc = 'Obsidian: Backlinks' },
-    { '<leader>od', '<cmd>ObsidianToday<cr>', desc = 'Obsidian: Today (daily)' },
+    },
   },
-  }
-  
+}
