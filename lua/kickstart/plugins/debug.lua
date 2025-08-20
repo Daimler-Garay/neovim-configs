0d4ad29141dd9bd902c732b7f032b403efa1158e
Daimler Garay <daimlerchryslergaray@gmail.com>
2025-08-19 20:00:07 -0500

    fixed python debugger finally

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -1,25 +1,15 @@
 return {
-  -- NOTE: Yes, you can install new plugins here!
   'mfussenegger/nvim-dap',
-  -- NOTE: And you can specify dependencies as well
   dependencies = {
-    -- Creates a beautiful debugger UI
     'rcarriga/nvim-dap-ui',
-
-    -- Required dependency for nvim-dap-ui
     'theHamsta/nvim-dap-virtual-text',
     'nvim-neotest/nvim-nio',
-
-    -- Installs the debug adapters for you
     'mason-org/mason.nvim',
     'jay-babu/mason-nvim-dap.nvim',
-
-    -- Add your own debuggers here
     'leoluz/nvim-dap-go',
     'mfussenegger/nvim-dap-python',
   },
   keys = {
-    -- Basic debugging keymaps, feel free to change to your liking!
     {
       '<F5>',
       function()
@@ -62,7 +52,6 @@ return {
       end,
       desc = 'Debug: Set Breakpoint',
     },
-    -- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
     {
       '<F7>',
       function()
@@ -70,35 +59,43 @@ return {
       end,
       desc = 'Debug: See last session result.',
     },
+
+    {
+      '<leader>dm',
+      function()
+        require('dap-python').test_method()
+      end,
+      desc = 'Debug: pytest method',
+    },
+    {
+      '<leader>dc',
+      function()
+        require('dap-python').test_class()
+      end,
+      desc = 'Debug: pytest class',
+    },
+    {
+      '<leader>ds',
+      function()
+        require('dap-python').debug_selection()
+      end,
+      desc = 'Debug: selection',
+    },
   },
   config = function()
-    local dap = require 'dap'
-    local dapui = require 'dapui'
+    local dap, dapui = require 'dap', require 'dapui'
 
     require('mason-nvim-dap').setup {
-      -- Makes a best effort to setup the various debuggers with
-      -- reasonable debug configurations
       automatic_installation = true,
-
-      -- You can provide additional configuration to the handlers,
-      -- see mason-nvim-dap README for more information
       handlers = {},
-
-      -- You'll need to check that you have the required things installed
-      -- online, please don't ask me how to install them :)
       ensure_installed = {
-        -- Update this to ensure that you have the debuggers for the langs you want
         'delve',
         'codelldb',
+        'python',
       },
     }
 
-    -- Dap UI setup
-    -- For more information, see |:help nvim-dap-ui|
     dapui.setup {
-      -- Set icons to characters that are more likely to work in every terminal.
-      --    Feel free to remove or use ones that you like more! :)
-      --    Don't feel like these are good choices.
       icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
       controls = {
         icons = {
@@ -114,31 +111,72 @@ return {
         },
       },
     }
-
-    -- Change breakpoint icons
-    -- vim.api.nvim_set_hl(0, 'DapBreak', { fg = '#e51400' })
-    -- vim.api.nvim_set_hl(0, 'DapStop', { fg = '#ffcc00' })
-    -- local breakpoint_icons = vim.g.have_nerd_font
-    --     and { Breakpoint = '', BreakpointCondition = '', BreakpointRejected = '', LogPoint = '', Stopped = '' }
-    --   or { Breakpoint = '●', BreakpointCondition = '⊜', BreakpointRejected = '⊘', LogPoint = '◆', Stopped = '⭔' }
-    -- for type, icon in pairs(breakpoint_icons) do
-    --   local tp = 'Dap' .. type
-    --   local hl = (type == 'Stopped') and 'DapStop' or 'DapBreak'
-    --   vim.fn.sign_define(tp, { text = icon, texthl = hl, numhl = hl })
-    -- end
+    require('nvim-dap-virtual-text').setup()
 
     dap.listeners.after.event_initialized['dapui_config'] = dapui.open
     dap.listeners.before.event_terminated['dapui_config'] = dapui.close
     dap.listeners.before.event_exited['dapui_config'] = dapui.close
 
-    -- Install golang specific config
     require('dap-go').setup {
-      delve = {
-        -- On Windows delve must be run attached or it crashes.
-        -- See https://github.com/leoluz/nvim-dap-go/blob/main/README.md#configuring
-        detached = vim.fn.has 'win32' == 0,
+      delve = { detached = vim.fn.has 'win32' == 0 },
+    }
+
+    local function python_path()
+      for _, v in ipairs { '.venv', 'venv', '.env' } do
+        local exe = (vim.fn.has 'win32' == 1) and (v .. '\\Scripts\\python.exe') or (v .. '/bin/python')
+        if vim.fn.executable(exe) == 1 then
+          return exe
+        end
+      end
+      if vim.fn.executable 'python3' == 1 then
+        return 'python3'
+      end
+      return 'python'
+    end
+
+    require('dap-python').setup(python_path())
+
+    local dap_py = require 'dap-python'
+    dap_py.test_runner = 'pytest'
+    dap_py.setup_args = { '--log-cli-level=INFO' }
+
+    dap.configurations.python = {
+      {
+        type = 'python',
+        request = 'launch',
+        name = 'Launch file',
+        program = '${file}',
+        cwd = '${workspaceFolder}',
+        console = 'integratedTerminal',
+        justMyCode = true,
+      },
+      {
+        type = 'python',
+        request = 'launch',
+        name = 'Module: run -m <module>',
+        module = function()
+          return vim.fn.input 'Module to run (e.g. package.module): '
+        end,
+        cwd = '${workspaceFolder}',
+        console = 'integratedTerminal',
+        justMyCode = true,
+      },
+      {
+        type = 'python',
+        request = 'attach',
+        name = 'Attach to process',
+        processId = require('dap.utils').pick_process,
+        justMyCode = false,
       },
     }
-    require('dap-python').setup 'python'
+
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = 'python',
+      callback = function()
+        dap.listeners.after.event_initialized['dapui_python_autopen'] = function()
+          dapui.open()
+        end
+      end,
+    })
   end,
 }
--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -7,7 +7,6 @@ return {
       local lint = require 'lint'
       lint.linters_by_ft = {
         markdown = { 'markdownlint' },
-        python = { 'pylint' },
         typescriptreact = { 'eslint_d' },
         typescript = { 'eslint_d' },
         json = { 'jsonlint' },
