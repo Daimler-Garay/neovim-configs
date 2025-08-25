a58a85ae7fbae80175a6dd1e57a6065b92041e08
Daimler Garay <daimlerchryslergaray@gmail.com>
2025-08-25 10:53:51 -0500

    - Added overseer & rewrote rustacean.nvim plugin

--- lua/custom/plugins/rustacean-nvim.lua
+++ lua/custom/plugins/rustacean-nvim.lua
@@ -1,88 +1,32 @@
 return {
-  {
-    'mrcjkb/rustaceanvim',
-    version = '^5',
-    lazy = false,
-    ft = { 'rust' },
-
-    opts = {
+  'mrcjkb/rustaceanvim',
+  version = '^4', -- keep in sync with stable
+  ft = { 'rust' },
+  config = function()
+    vim.g.rustaceanvim = {
       server = {
-        on_attach = function(_, bufnr)
-          vim.keymap.set('n', '<leader>cR', function()
-            vim.cmd.RustLsp 'codeAction'
-          end, { desc = 'Code Action', buffer = bufnr })
-          vim.keymap.set('n', '<leader>cR', function()
-            vim.cmd.RustLsp 'codeAction'
-          end, { desc = 'Code Action', buffer = bufnr })
+        on_attach = function(client, bufnr)
+          local keymap = vim.keymap.set
+          local opts = { buffer = bufnr, silent = true, noremap = true }
 
-          vim.keymap.set('n', '<leader>rd', function()
-            vim.cmd.RustLsp 'debuggables'
-          end, { desc = 'Rust Debuggables', buffer = bufnr })
-
-          vim.keymap.set('n', '<leader>rl', function()
-            vim.cmd.RustLsp 'runnables'
-          end, { desc = 'Rust Runnables', buffer = bufnr })
-
-          vim.keymap.set('n', '<leader>rr', function()
-            vim.cmd.RustLsp 'run'
-          end, { desc = 'Rust Run', buffer = bufnr })
-
-          vim.keymap.set('n', '<leader>rs', function()
-            vim.cmd.RustLsp 'testables'
-          end, { desc = 'Rust Testables', buffer = bufnr })
-
-          vim.keymap.set('n', '<leader>ro', function()
-            vim.cmd.RustLsp 'openDocs'
-          end, { desc = 'Rust OpenDocs', buffer = bufnr })
-
-          vim.keymap.set('n', '<leader>rf', function()
-            vim.cmd.RustLsp 'flyCheck'
-          end, { desc = 'Rust FlyCheck', buffer = bufnr })
-
-          vim.keymap.set('n', '<leader>rd', function()
-            vim.cmd.RustLsp 'renderDiagnostic'
-          end, { desc = 'Rust Render Diagnostics', buffer = bufnr })
-
-          vim.keymap.set('n', '<leader>rdd ', function()
-            vim.cmd.RustLsp { 'renderDiagnostic', 'cycle' }
-          end, { desc = 'Rust Diagnostics All', buffer = bufnr })
-
-          vim.keymap.set('n', '<leader>rdc ', function()
-            vim.cmd.RustLsp { 'renderDiagnostic', 'current' }
-          end, { desc = 'Rust Diagnostic Current', buffer = bufnr })
+          keymap('n', 'K', vim.lsp.buf.hover, opts)
+          keymap('n', 'gd', vim.lsp.buf.definition, opts)
+          keymap('n', 'gr', vim.lsp.buf.references, opts)
+          keymap('n', '<leader>rn', vim.lsp.buf.rename, opts)
+          keymap('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+          keymap('n', '<leader>f', function()
+            vim.lsp.buf.format { async = true }
+          end, opts)
         end,
         default_settings = {
           ['rust-analyzer'] = {
-            cargo = {
-              allFeatures = true,
-              loadOutDirsFromCheck = true,
-              buildScripts = {
-                enable = true,
-              },
-            },
-            checkOnSave = false,
-            procMacro = {
-              enable = true,
-              ignored = {
-                ['napi-derive'] = { 'napi' },
-              },
-            },
-            inlayHints = {
-              enabled = false,
-            },
-            diagnostics = {
-              disabled = { 'inactive-code' },
-            },
+            cargo = { allFeatures = true },
           },
         },
       },
-    },
-    config = function(_, opts)
-      vim.g.rustaceanvim = vim.tbl_deep_extend('keep', vim.g.rustaceanvim or {}, opts or {})
-      if vim.fn.executable 'rust-analyzer' == 0 then
-        LazyVim.error('**rust-analyzer** not found in PATH, please install it.\nhttps://rust-analyzer.github.io/', { title = 'rustaceanvim' })
-      end
-    end,
-  },
-  {},
+      tools = {
+        hover_actions = { auto_focus = true },
+      },
+    }
+  end,
 }
