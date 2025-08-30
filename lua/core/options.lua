f0b2e3298d68ac881d0e44ddd0d5d61857a128ee
Daimler Garay <daimlerchryslergaray@gmail.com>
2025-08-29 23:14:40 -0500

    saving

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,62 +1,38 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
-
 vim.opt.mouse = 'a'
-
--- Don't show the mode, since it's already in the status line
 vim.opt.showmode = false
-
--- Sync clipboard between OS and Neovim.
---  Schedule the setting after `UiEnter` because it can increase startup-time.
---  Remove this option if you want your OS clipboard to remain independent.
---  See `:help 'clipboard'`
-vim.schedule(function()
-  vim.opt.clipboard = 'unnamedplus'
-end)
-
--- Enable break indent
 vim.opt.breakindent = true
-
--- Save undo history
 vim.opt.undofile = true
-
--- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
-
--- Keep signcolumn on by default
 vim.opt.signcolumn = 'yes'
-
--- Decrease update time
 vim.opt.updatetime = 250
-
--- Decrease mapped sequence wait time
 vim.opt.timeoutlen = 300
-
--- Configure how new splits should be opened
 vim.opt.splitright = true
 vim.opt.splitbelow = true
-
--- Sets how neovim will display certain whitespace characters in the editor.
---  See `:help 'list'`
---  and `:help 'listchars'`
 vim.opt.list = true
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
-
--- Preview substitutions live, as you type!
 vim.opt.inccommand = 'split'
-
--- Show which line your cursor is on
 vim.opt.cursorline = true
-
--- Minimal number of screen lines to keep above and below the cursor.
-vim.opt.scrolloff = 10
-
--- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
--- instead raise a dialog asking if you wish to save the current file(s)
--- See `:help 'confirm'`
+vim.opt.scrolloff = 8
+vim.o.sidescrolloff = 8
 vim.opt.confirm = true
 vim.o.swapfile = false
+vim.o.conceallevel = 1
+vim.o.wrap = false
+vim.o.smartindent = true
+vim.o.expandtab = true
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.softtabstop = 4
+vim.opt.termguicolors = true -- True color support
+vim.opt.timeoutlen = 500 -- Mapped sequence timeout
+vim.opt.ttimeoutlen = 0 -- Faster escape key response
+vim.opt.lazyredraw = true -- Don’t redraw while executing macros
+vim.opt.clipboard = 'unnamedplus' -- Use system clipboard
+vim.opt.completeopt = { 'menuone', 'noselect' } -- Better completion
+vim.opt.pumheight = 10 -- Popup menu height
 
 --- Autosave file when leaving insert mode
 local autosave_filetypes = {
@@ -80,5 +56,4 @@ vim.api.nvim_create_autocmd({ 'InsertLeave', 'TextChanged', 'TextChangedI' }, {
   end,
 })
 
-vim.o.conceallevel = 1
 -- vim: ts=2 sts=2 sw=2 et
--- lua/kickstart/plugins/indent_line.lua
+++ lua/kickstart/plugins/indent_line.lua
@@ -12,28 +12,28 @@ return {
         show_exact_scope = false,
       },
       exclude = {
-        filetypes = {
-          'help',
-          'terminal',
-          'lazy',
-          'lspinfo',
-          'TelescopePrompt',
-          'TelescopeResults',
-          'gitcommit',
-          'markdown',
-          'txt',
-          'log',
-          'alpha',
-          'dashboard',
-          '',
-        },
-        buftypes = {
-          'terminal',
-          'nofile',
-          'quickfix',
-          'prompt',
-        },
+        filetypes = {},
       },
     },
+    config = function(_, opts)
+      local indent_filetypes = {
+        'python',
+        'yaml',
+        'nim',
+        'make',
+      }
+
+      require('ibl').setup(opts)
+
+      vim.api.nvim_create_autocmd('FileType', {
+        callback = function(event)
+          if vim.tbl_contains(indent_filetypes, event.match) then
+            require('ibl').setup_buffer(0, opts)
+          else
+            require('ibl').setup_buffer(0, { enabled = false })
+          end
+        end,
+      })
+    end,
   },
 }
